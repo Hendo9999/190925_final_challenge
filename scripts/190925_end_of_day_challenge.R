4ec5f345c86ab12eeb49b0de8e0af1557694eb75
Henderson <hen232@csiro.au>
2019-09-25 16:17:06 +0930

    question 1 added

--- scripts/190925_end_of_day_challenge.R
+++ scripts/190925_end_of_day_challenge.R
@@ -3,5 +3,20 @@
 library(tidyverse)
 
 BOM_data <- read_csv("data/BOM_data.csv")
-BOM_stations <- read.csv2("data/BOM_stations.csv")
+BOM_stations <- read_csv("data/BOM_stations.csv")
 
+#For each station, how many days have a minimum temperature, 
+#a maximum temperature and a rainfall measurement recorded?
+
+#convert - to NA
+#count number of rows with Rainfall 
+#separate Temp_min_max first
+#count number of rows with number of values
+
+test_NA_conversion <- BOM_data
+test1 <- na_if(test_NA_conversion, c("-/-", "-"))
+test1
+
+
+n
+na_if()
