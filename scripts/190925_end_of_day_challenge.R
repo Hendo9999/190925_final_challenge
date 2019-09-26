d52a41cbc95ac327d91e176ecc8860a2d734cd68
Henderson <hen232@csiro.au>
2019-09-26 14:23:24 +0930

    start challenge 2

--- scripts/190925_end_of_day_challenge.R
+++ scripts/190925_end_of_day_challenge.R
@@ -8,48 +8,27 @@ BOM_stations <- read_csv("data/BOM_stations.csv")
 #For each station, how many days have a minimum temperature, 
 #a maximum temperature and a rainfall measurement recorded?
 
-#separate BOM_data temp min max columns
-BOM_data_separate <- separate(BOM_data_grouped_station, col=Temp_min_max, into = c("temp_min", "temp_max"), sep = "/")
-
-#rainfall measurement recorded
-BOM_rainfall_data<- filter(BOM_data_separate, Rainfall != "-", temp_min != "-", temp_max != "-")
-BOM_data_grouped_station <- group_by(BOM_rainfall_data, Station_number)
-summarise(BOM_data_grouped_station,  num_rows=n())
-
-
-#as above with pipes
-separate(BOM_data_grouped_station, col=Temp_min_max, into = c("temp_min", "temp_max"), sep = "/") %>%
+BOM_data_challenge1<- separate(BOM_data, col=Temp_min_max, into = c("temp_min", "temp_max"), sep = "/") %>%
   filter(Rainfall != "-", temp_min != "-", temp_max != "-") %>%
   group_by(Station_number) %>%
-  summarise(BOM_data_grouped_station, num_rows=n())
+  summarise(num_rows=n())
 
 
+#Challenge2
+#Which month saw the lowest average daily temperature difference?
+#In addition to the functions you used above, 
+  #this question will need a mutate() to calculate the temperature difference.
+#The temperature values are stored as characters after you have run separate() 
+  #(see the <chr> in the second row if you print the data frame to the console).
+#To be able to calculate the difference without an error, 
+  #you will need to convert them to numeric values with as.numeric() first.
 
+?as.numeric()
 
-Bom_temp_min <- filter(BOM_data_separate, temp_min != "-")
-head(test2)
-?count()
-BOM_data_separate           
-?is.numeric()
-  o
-?filter()  
+BOM_data_challenge2<- separate(BOM_data, col=Temp_min_max, into = c("temp_min", "temp_max"), sep = "/") %>%
+  mutate
+  
+  as.numeric()
 
-?is.numeric()
-
-mutate
-
-head(BOM_data_separate)
-?separate
-
-BOM_data
-BOM_stations
-rm(test1)
-?sub()
 BOM_data
 head(BOM_data)
-grep("-", test_NA_conversion)
-?arrange
-arrange(BOM_data, Rainfall)
-tail(BOM_data)
-
-as.numeric(c("-.1"," 2.7 ","B")) # (-0.1, 2.7, NA) try this code
