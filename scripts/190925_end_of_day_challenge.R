a9e4ef1ffab09b8d1f1e36a1bacf385d874f93aa
Henderson <hen232@csiro.au>
2019-09-26 13:53:34 +0930

    Finished Challenge 1

--- scripts/190925_end_of_day_challenge.R
+++ scripts/190925_end_of_day_challenge.R
@@ -8,15 +8,48 @@ BOM_stations <- read_csv("data/BOM_stations.csv")
 #For each station, how many days have a minimum temperature, 
 #a maximum temperature and a rainfall measurement recorded?
 
-#convert - to NA
-#count number of rows with Rainfall 
-#separate Temp_min_max first
-#count number of rows with number of values
+#separate BOM_data temp min max columns
+BOM_data_separate <- separate(BOM_data_grouped_station, col=Temp_min_max, into = c("temp_min", "temp_max"), sep = "/")
 
-test_NA_conversion <- BOM_data
-test1 <- na_if(test_NA_conversion, c("-/-", "-"))
-test1
+#rainfall measurement recorded
+BOM_rainfall_data<- filter(BOM_data_separate, Rainfall != "-", temp_min != "-", temp_max != "-")
+BOM_data_grouped_station <- group_by(BOM_rainfall_data, Station_number)
+summarise(BOM_data_grouped_station,  num_rows=n())
 
 
-n
-na_if()
+#as above with pipes
+separate(BOM_data_grouped_station, col=Temp_min_max, into = c("temp_min", "temp_max"), sep = "/") %>%
+  filter(Rainfall != "-", temp_min != "-", temp_max != "-") %>%
+  group_by(Station_number) %>%
+  summarise(BOM_data_grouped_station, num_rows=n())
+
+
+
+
+Bom_temp_min <- filter(BOM_data_separate, temp_min != "-")
+head(test2)
+?count()
+BOM_data_separate           
+?is.numeric()
+  o
+?filter()  
+
+?is.numeric()
+
+mutate
+
+head(BOM_data_separate)
+?separate
+
+BOM_data
+BOM_stations
+rm(test1)
+?sub()
+BOM_data
+head(BOM_data)
+grep("-", test_NA_conversion)
+?arrange
+arrange(BOM_data, Rainfall)
+tail(BOM_data)
+
+as.numeric(c("-.1"," 2.7 ","B")) # (-0.1, 2.7, NA) try this code
